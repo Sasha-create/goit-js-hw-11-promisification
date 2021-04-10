f30ed862b0d37494c30944911c56b180e843a66c
Aleksandra Knott <71516075+Sasha-create@users.noreply.github.com>
2021-04-10 19:37:54 +0100

    task-2

---
+++ task-2.js
@@ -0,0 +1,26 @@
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  const updatedUsers = allUsers.map((user) =>
+    user.name === userName ? { ...user, active: !user.active } : user
+  );
+  return Promise.resolve(updatedUsers);
+};
+
+//async
+// const toggleUserState = async (allUsers, userName) =>
+//   allUsers.map((user) =>
+//     user.name === userName ? { ...user, active: !user.active } : user
+//   );
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+
+/* Должно работать так*/
+
+// toggleUserState(users, "Mango").then(logger);
+// toggleUserState(users, "Lux").then(logger);
